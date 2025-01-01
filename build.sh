f863beefdaa14fa0bb7d0e17079af93914abe42f
Arthur <165717741+Truc1532@users.noreply.github.com>
2025-01-01 17:01:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# compiling
 echo "Compiling..."
 nasm -f elf32 -o loader.o src/assembly/loader.asm
 gcc -m32 -o main.o -c src/main.c -nostdlib -nostartfiles -nodefaultlibs
@@ -6,8 +7,16 @@ gcc -m32 -o commands.o -c src/commands.c -nostdlib -nostartfiles -nodefaultlibs
 gcc -m32 -o date\&time.o -c src/date\&time.c -nostdlib -nostartfiles -nodefaultlibs
 gcc -m32 -o cpuinfo.o -c src/cpuinfo.c -nostdlib -nostartfiles -nodefaultlibs
 
+# linking
 echo "Linking..."
 gcc -m32 -r globals.o date\&time.o cpuinfo.o commands.o main.o -o kernel.o -nostdlib -nostartfiles -nodefaultlibs
 ld -T linker.ld -m elf_i386 -o kernel.efi loader.o kernel.o
 
-echo "Done, you can create the iso with the instruction in the README.txt file"
+# creating the iso
+cp kernel.efi isodir/boot/
+
+grub-mkrescue -o kernel.iso isodir/
+
+# clean up
+mkdir cache
+mv loader.o main.o globals.o commands.o date\&time.o cpuinfo.o cache/
